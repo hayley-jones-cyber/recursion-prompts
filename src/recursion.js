542d9f5d519073b33a958343721172af9da02238
hayley jones <joneshayley991@gmail.com>
2020-11-06 16:55:48 -0800

    1st and 2nd problems complete with passing tests, third problem has psudocode written.

--- src/recursion.js
+++ src/recursion.js
@@ -7,16 +7,34 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+  if (n < 0) {
+    return null;
+  }
+  if (n === 0) {
+    return 1;
+  }
+  return n*(factorial(n-1));
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+  if (array.length === 0) {
+    return 0;
+  } else {
+    firtArrayValue = array[0];
+    return array[0] + sum(array.slice(1));
+  }
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+  //base case, empty arr, return 0
+  //recursion case, not an empty arr
+    //variable to hold the sum of the inner function
+    //inner function to flatten
+    //iterate over the flattend function
 };
 
 // 4. Check if a number is even.
